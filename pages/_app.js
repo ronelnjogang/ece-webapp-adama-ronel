775f2ea865b5ea71aa02a1cbd0208353fd0b0a88
Ece-Webapp-Adama-Kouyate <113826061+Ece-Webapp-Adama-Kouyate@users.noreply.github.com>
2022-12-26 16:35:01 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,21 +2,26 @@ import "bootstrap/dist/css/bootstrap.css"
 import "../styles/style.css"
 import Layout from "../components/layout"
 
-
-import '../styles/globals.css'
-
-import { useEffect } from 'react';
-
-function MyApp({ Component, pageProps }) {
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { useState, useEffect } from 'react';
+import Account from "../components/account"
+export default function MyApp({ Component, pageProps }) {
+  const [supabase] = useState(() => createBrowserSupabaseClient())
   useEffect(()=>{
     import("bootstrap/dist/js/bootstrap");
 },[]);
 
   return (
+
   <Layout>
-    <Component {...pageProps} />
+    
+    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
+      <Component {...pageProps} />
+    </SessionContextProvider>
   </Layout>)
+
+
   
 }
 
-export default MyApp
