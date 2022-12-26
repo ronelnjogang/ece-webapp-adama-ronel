cf71fdc2296b7803f216dbc03ea04e1c7f42286f
Ece-Webapp-Adama-Kouyate <113826061+Ece-Webapp-Adama-Kouyate@users.noreply.github.com>
2022-12-26 17:31:30 +0100

    Resolved conflits

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import "bootstrap/dist/css/bootstrap.css"
 import "../styles/style.css"
 import Layout from "../components/layout"
-
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import { useState, useEffect } from 'react';
 import Account from "../components/account"
+import { ThemeProvider } from "next-themes";
+import { MyThemeContextProvider } from "./themeSwitch.tsx";
 export default function MyApp({ Component, pageProps }) {
   const [supabase] = useState(() => createBrowserSupabaseClient())
   useEffect(()=>{
@@ -15,10 +16,11 @@ export default function MyApp({ Component, pageProps }) {
   return (
 
   <Layout>
-    
+     <MyThemeContextProvider>
     <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
       <Component {...pageProps} />
     </SessionContextProvider>
+    </MyThemeContextProvider>
   </Layout>)
 
 
